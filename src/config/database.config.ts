b1b8314e1d920e153f341f39b59d2aed661677a0
Desarrollo-Prime <desarrollo1@primedigitale.com>
2025-08-13 23:40:46 -0500

    variables de entorno local

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -9,10 +9,10 @@ config();
 const databaseConfig = (): TypeOrmModuleOptions => ({
   type: 'postgres',
   host: process.env.DB_HOST || 'localhost',
-  port: Number(process.env.DB_PORT || '5432'), // Usar Number() para asegurar el tipo
-  username: process.env.DB_USERNAME || 'postgres',
-  password: process.env.DB_PASSWORD || '1234',
-  database: process.env.DB_DATABASE || 'bigc',
+  port: Number(process.env.DB_PORT || process.env.DB_LOCAL_PORT), // Usar Number() para asegurar el tipo
+  username: process.env.DB_USERNAME || process.env.DB_LOCAL_USERNAME,
+  password: process.env.DB_PASSWORD || process.env.DB_LOCAL_PASSWORD,
+  database: process.env.DB_DATABASE || process.env.DB_LOCAL_DATABASE,
   entities: [join(__dirname, '..', '**', '*.entity.{ts,js}')], // Cargar todas las entidades
   synchronize: false, // ¡IMPORTANTE! NUNCA usar `true` en producción. Se asume que el esquema ya está creado.
   logging: true, // Habilita el log de las consultas SQL (útil para desarrollo)
